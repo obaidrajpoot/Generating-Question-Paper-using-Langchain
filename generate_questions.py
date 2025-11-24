b7b80c2794b1446757592c8b90632f29645dc6a6
Obaid Ullah <rajputobaid024@gmail.com>
2025-11-24 16:34:25 +0300

    Update generate_questions.py

--- generate_questions.py
+++ generate_questions.py
@@ -29,7 +29,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatOpenAI(model = 'google/gemma-3n-e2b-it:free',
-                   api_key='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',
+                   api_key=['Yours api key'],
                    base_url="https://openrouter.ai/api/v1")
 
 
@@ -38,5 +38,6 @@ def generate_question(level,short_ques,long_ques,mcq_ques,fill_ques):
      final_prompt =questions_prompt.format(pdf_text=pdf_text,difficulty=level,
                                          short_size=short_ques,long_size=long_ques,
                                            mcq_size=mcq_ques,fill_size=fill_ques  )
+
 
      return model.invoke(final_prompt).content
